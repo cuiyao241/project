81019a5e011c01218e92af3bc28912a8b210499d
lizihao7654321 <120985357@qq.com>
2017-03-09 00:05:56 +0800

    修改

--- project/resources/views/layout/home.blade.php
+++ project/resources/views/layout/home.blade.php
@@ -171,6 +171,48 @@
 				</div>
 				<div class="clearfix"></div>
 			</div>
+
+			<!-- Popup-Box -->
+			<div id="popup1">
+				<div id="small-dialog1" class="mfp-hide agileinfo">
+					<div class="pop_up">
+						<form action="#" method="post">
+							<h3>LOGIN</h3>
+							<input type="text" Name="Userame" placeholder="Username" required="">
+							<input type="password" Name="Password" placeholder="Password" required="">
+							<ul class="tick w3layouts agileinfo">
+								<li>
+									<input type="checkbox" id="brand1" value="">
+									<label for="brand1"><span></span>Remember me</label>
+								</li>
+								<li>
+									<a href="#">Forgot Password?</a>
+								</li>
+							</ul>
+							<div class="send-button wthree agileits">
+								<input type="submit" value="LOGIN">
+							</div>
+						</form>
+					</div>
+				</div>
+				<div id="small-dialog2" class="mfp-hide agileinfo">
+					<div class="pop_up">
+						<form action="#" method="post">
+							<h3>SIGN UP</h3>
+							<input type="text" Name="Name" placeholder="Name" required="">
+							<input type="text" Name="Email" placeholder="Email" required="">
+							<input type="password" Name="Password" placeholder="Password" required="">
+							<input type="text" Name="Phone Number" placeholder="Phone Number" required="">
+							<div class="send-button wthree agileits">
+								<input type="submit" value="SIGN UP">
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<!-- //Popup-Box -->			
+
+
 		</div>
 		<!-- //Header-Top-Bar-(Hidden) -->
 	
