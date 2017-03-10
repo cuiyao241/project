7a30f6a88a8efbf6904790fe20836d015b5c0a24
CuiYao <cuiyao241@163.com>
2017-03-10 15:32:25 +0800

    dingdan

--- project/app/Http/Controllers/HomeCartController.php
+++ project/app/Http/Controllers/HomeCartController.php
@@ -17,28 +17,30 @@ class HomeCartController extends Controller
     	$res = array('color'=>'红色','size'=>'xxl','num'=>'5','id'=>'13');
     	Session::push('cart',$res);
     }
-    public function getTwo()
-    {	
-    	$res[] = null;
-    	$res = array('color'=>'蓝色','size'=>'xl','num'=>'2','id'=>'14');
-    	Session::push('cart',$res);
-    }
-    public function getTh()
-    {	
-    	$res[] = null;
-    	$res = array('color'=>'棕色','size'=>'xxxl','num'=>'1','id'=>'16');
-    	Session::push('cart',$res);
-    }
-    //加入购物车提醒
-    // public function postRemind(Request $request)
-    // {
-    // 	$res = $request->except('_token');
-    // 	//把数据存入到数据库
+    // public function getTwo()
+    // {	
+    // 	$res[] = null;
+    // 	$res = array('color'=>'蓝色','size'=>'xl','num'=>'2','id'=>'14');
     // 	Session::push('cart',$res);
-
-    // 	return view('homes.cart.remind');
     // }
-    //
+    // public function getTh()
+    // {	
+    // 	$res[] = null;
+    // 	$res = array('color'=>'棕色','size'=>'xxxl','num'=>'1','id'=>'16');
+    // 	Session::push('cart',$res);
+    // }
+    //加入购物车提醒
+    public function postRemind(Request $request)
+    {
+    	$res = $request->except('_token');
+    	//把数据存入到数据库
+    	// Session::push('cart',$res);
+        // dd($res);
+
+    	return view('homes.cart.remind');
+    }
+
+    //购物车
     public function getShopcart()
     {
     	$data = Session::get('cart');
