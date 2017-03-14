947aa822902008d3d36ad070833476c2e701cb87
Your Name <email@example.com>
2017-03-14 22:14:57 +0800

    wjx

--- project/resources/views/homes/pers/Setpassword.blade.php
+++ project/resources/views/homes/pers/Setpassword.blade.php
@@ -12,34 +12,32 @@
   <div class="settings_box"> 
    <strong class="settings_title">用户密码</strong> 
    <fieldset class="setPersonal"> 
-    <form  method="post"  action="/home/personal/pwdupdate/20"> 
+    <form  method="post"  action="/home/personal/pwdupdate/69"> 
       {{ csrf_field() }}
       @if (count($errors) > 0)
-               <div class="mws-form-message error">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li style='font-size:5px;list-style:none'>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-     <div class="form-list" id="old-form"> 
-      <label class="account-label">当前密码</label> 
-      <input style="display:none" />
+         <div class="mws-form-message error">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li style='font-size:5px;list-style:none'>{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+      @endif
+    <div class="form-list"> 
+      <label class="account-label">旧密码</label> 
+ 
       <!-- for disable autocomplete on chrome --> 
-      <input autocomplete="off" class="l_ipt"  name="
-      Password" type="password" value="{{$data->Password}}"  disabled/> 
-      <strong style="display:none;"></strong> 
-      <!-- <a id="forget"><strong>忘记密码？</strong></a> --> 
+      <input  class="l_ipt" name="oldPassword" type="password" value="" /> 
+      <strong></strong> 
      </div> 
+
      <div class="form-list"> 
       <label class="account-label">新密码</label> 
-      <input style="display:none" />
+  
       <!-- for disable autocomplete on chrome --> 
       <input autocomplete="off" class="l_ipt" name="Password" type="password" value="" /> 
       <strong></strong> 
      </div> 
-     <div class="pw_safe none_f" style="display: block;"> <span class="txt">安全程度</span> <div class="pw_strength pw_weak pw_medium pw_strong"> <div class="pw_bar"></div> <div class="pw_letter"> <span class="strength_l pw_strength_color">低</span> <span class="strength_m">中</span> <span class="strength_h">强</span> </div> </div> </div>
      <div class="form-list"> 
       <label class="account-label">确认密码</label> 
       <input style="display:none" />
@@ -58,6 +56,77 @@
 @endsection
 
 @section('js')
+<script type="text/javascript">
+// alert($);
+ //全局变量
+  var PU = false;
+  var RPU = false;
 
- @show
+  //密码
+  //获取焦点
+  $('input[name=Password]').focus(function(){
+    $(this).css('border','solid 1px #999');
+    $(this).next().text(' *请输入新密码').css('color','black');
+    $(this).addClass('cur');
+  })
+  //失去焦点
+  $('input[name=Password]').blur(function(){
+    //获取输入密码的值
+    var pv = $(this).val();
+
+    //正则表达式
+    var reg = /^\w{5,12}$/;
+    //检测
+    var pvs = reg.test(pv);
+
+    if(!pvs){
+
+      $(this).css('border','solid 1px red');
+      $(this).next().text(' *密码格式不正确').css('color','red');
+
+      PU = false;
+    } else {
+
+      $(this).css('border','solid 1px green');
+      $(this).next().text(' √').css('color','green');
+
+      PU = true;
+
+    }
+  })
+
+  //确认密码
+  $('input[name=rePassword]').focus(function(){
+    $(this).css('border','solid 1px #999');
+    $(this).next().text(' *请输入确认密码').css('color','black');
+    $(this).addClass('cur');
+  })
+
+  //失去焦点
+  $('input[name=rePassword]').blur(function(){
+
+    //获取确认密码值
+    var rpv = $(this).val();
+    //获取输入密码的值
+    var pv = $('input[name=Password]').val();
+
+    if(rpv != pv){
+
+      $(this).css('border','solid 1px red');
+      $(this).next().text(' *两次密码不一致').css('color','red');
+
+      RPU = false;
+
+    } else {
+      $(this).css('border','solid 1px green');
+      $(this).next().text(' √').css('color','green');
+
+      RPU = true;
+
+
+    }
+  })
+
+</script>
+@show
 
